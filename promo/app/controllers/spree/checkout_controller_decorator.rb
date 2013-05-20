bb41b5ba0352ee895337b0be12cca564c1e293ce
PiotrKarbownik <piotr@piotr-K75VM.(none)>
2013-05-20 09:35:33 +0200

    zero value checkout

--- promo/app/controllers/spree/checkout_controller_decorator.rb
+++ promo/app/controllers/spree/checkout_controller_decorator.rb
@@ -22,6 +22,8 @@ Spree::CheckoutController.class_eval do
       end
 
       if @order.state == 'complete' || @order.completed?
+        session[:order_id] = nil 
+        @order.update_attributes({:state => "complete", :payment_state => 'paid', :completed_at => Time.now}, :without_protection => true)
         flash.notice = t(:order_processed_successfully)
         flash[:commerce_tracking] = 'nothing special'
         respond_with(@order, :location => completion_route)
