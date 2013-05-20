3afef2ab7a0a1124ca16cd6b4cb4a82a6ef04332
PiotrKarbownik <piotr@piotr-K75VM.(none)>
2013-05-20 09:56:56 +0200

    zero value checkout

--- promo/app/controllers/spree/checkout_controller_decorator.rb
+++ promo/app/controllers/spree/checkout_controller_decorator.rb
@@ -1,6 +1,12 @@
 Spree::CheckoutController.class_eval do
 
   #TODO 90% of this method is duplicated code. DRY
+  def edit
+    if @order.total==0 && spree_current_user && @order.state == 'complete'
+      session[:order_id] = nil 
+      @order.update_attributes({:state => "complete", :payment_state => 'paid', :completed_at => Time.now}, :without_protection => true)
+    end
+  end
   def update
     if @order.update_attributes(object_params)
 
@@ -31,10 +37,6 @@ Spree::CheckoutController.class_eval do
         respond_with(@order, :location => checkout_state_path(@order.state))
       end
     else
-      if @order.total==0 && spree_current_user && @order.state == 'complete'
-        session[:order_id] = nil 
-        @order.update_attributes({:state => "complete", :payment_state => 'paid', :completed_at => Time.now}, :without_protection => true)
-      end
       respond_with(@order) { |format| format.html { render :edit } }
     end
   end
